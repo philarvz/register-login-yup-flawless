095bca3f2049abcbb568b929eed6510ce4884d07
M4ltin12 <20233tn170@utez.edu.mx>
2025-03-14 20:16:47 -0600

    [UPDATE] login-ui

--- src/modules/login/screens/Login.jsx
+++ src/modules/login/screens/Login.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react";
+import "../../../../src/index.css";
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <>
+      <div className="container">
+        <h4 style={{ color: "gray" }}>
+          <span>Flawless</span>
+        </h4>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/599/599502.png"
+          alt="flawless"
+          style={{ width: 60, marginBottom: 10 }}
+        />
+        <form>
+          <label>Correo Electrónico:</label>
+          <input className="input" type="email" placeholder="Correo electrónico" />
+          <p>error...</p>
+
+          <label>Contraseña:</label>
+          <input className="input" type="password" placeholder="Contraseña" />
+          <p>error...</p>
+
+          <input
+            className="input"
+            style={{ backgroundColor: "rgb(144, 118, 99)", marginBottom: 10 }}
+            type="submit"
+            value="Iniciar sesión"
+          />
+
+          <p>
+            ¿No tienes una cuenta?{" "}
+            <a href="#" style={{ color: "rgb(144, 118, 99)", textDecoration: "none" }}>
+              Regístrate aquí
+            </a>
+          </p>
+        </form>
+      </div>
+    </>
+  );
 }
